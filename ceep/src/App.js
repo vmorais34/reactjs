1d57e6a4447fff87ecaf3c54a48bc6462d8b03f0
vmorais34 <vmorais34@gmail.com>
2021-01-10 13:48:46 -0300

    Primeiro lote de arquivos
    Teste

---
+++ ceep/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import ListaDeNotas from "./components/ListaDeNotas"
+
+function App() {
+  return (
+  // Temos de sempre retornar uma unica tag pai para funcionar
+ 
+
+export default App;
---
+++ ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+export default class ListaDeNotas extends Component {
+    render(){
+        return (
+            <ul>
+                <li>
+                    <p>Olá</p>
+                </li>
+            </ul>
+        );
+    }
+}
